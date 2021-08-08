f89b122c1b8cf2f76678363804e82e1ef1006892
YunYouJun <me@yunyoujun.cn>
2021-08-08 17:59:26 +0800

    :label: use typescript & upgrade gulp to ts

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,16 +1,23 @@
-const gulp = require("gulp"),
-  sass = require("gulp-sass"),
-  browserSync = require("browser-sync").create(),
-  reload = browserSync.reload,
-  colors = require("colors"),
-  del = require("del"),
-  autoprefixer = require("gulp-autoprefixer"),
-  cleanCSS = require("gulp-clean-css"),
-  notify = require("gulp-notify"),
-  plumber = require("gulp-plumber"),
-  rename = require("gulp-rename"),
-  size = require("gulp-size"),
-  concat = require("gulp-concat");
+import gulp from "gulp";
+import gulpSass from "gulp-sass";
+import dartSass from "sass";
+import del from "del";
+import autoprefixer from "gulp-autoprefixer";
+import cleanCSS from "gulp-clean-css";
+import notify from "gulp-notify";
+import plumber from "gulp-plumber";
+import rename from "gulp-rename";
+import size from "gulp-size";
+import concat from "gulp-concat";
+
+import chalk from "chalk";
+import { Logger } from "@yunyoujun/logger";
+const logger = new Logger();
+
+import browserSync from "browser-sync";
+
+const sass = gulpSass(dartSass);
+const reload = browserSync.create().reload;
 
 const themes = ["yun", "planet", "blood", "earth"];
 
@@ -28,23 +35,6 @@ const demo = {
   css: "css/common.css",
 };
 
-const sassOptions = {
-  outputStyle: "expanded",
-};
-
-colors.setTheme({
-  silly: "rainbow",
-  input: "grey",
-  verbose: "cyan",
-  prompt: "grey",
-  info: "green",
-  data: "grey",
-  help: "cyan",
-  warn: "yellow",
-  debug: "blue",
-  error: "red",
-});
-
 let onError = function (err) {
   notify.onError({
     title: "Gulp",
@@ -55,26 +45,35 @@ let onError = function (err) {
   this.emit("end");
 };
 
-function fileArray(theme) {
+function fileArray(theme: string) {
   return [
     bases.scss + "theme/_" + theme + ".scss",
     bases.scss + "base/_" + "markdown" + ".scss",
   ];
 }
 
-function scss(theme) {
+function scss(theme: string) {
   return gulp
     .src(fileArray(theme))
     .pipe(plumber({ errorHandler: onError }))
     .pipe(concat({ path: theme + "-markdown" + ".scss" }))
-    .pipe(sass(sassOptions).on("error", sass.logError))
+    .pipe(
+      sass
+        .sync({
+          outputStyle: "expanded",
+        })
+        .on("error", sass.logError)
+    )
     .pipe(size({ gzip: true, showFiles: true }))
     .pipe(autoprefixer())
     .pipe(gulp.dest(bases.dist + theme + "/"))
     .pipe(
-      cleanCSS({ debug: true }, function (details) {
-        console.log(details.name + ": " + details.stats.originalSize + " B");
-        console.log(details.name + ": " + details.stats.minifiedSize + " B");
+      cleanCSS({}, (details) => {
+        logger.info(
+          `${chalk.cyan(details.name)}: [${chalk.yellow(
+            details.stats.originalSize / 1000
+          )} KB] -> [${chalk.green(details.stats.minifiedSize / 1000)} KB]`
+        );
       })
     )
     .pipe(rename({ suffix: ".min" }))
@@ -93,11 +92,15 @@ function scssAll() {
   );
 }
 
-function clean() {
+/**
+ * clean dist
+ * @returns
+ */
+export function clean() {
   return del(["dist"]);
 }
 
-function watch() {
+export function watch() {
   browserSync.init({
     server: {
       baseDir: bases.demo,
@@ -115,12 +118,13 @@ function watch() {
     .on("change", reload);
 }
 
-function build() {
+/**
+ * clean and build scss
+ * @returns
+ */
+export function build() {
   clean();
   return scssAll();
 }
 
-exports.clean = clean;
-exports.watch = watch;
-exports.build = build;
 exports.default = gulp.series(build, watch);
